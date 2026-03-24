02f120dc7c5d61fb882b94dc0ee3b39954e598ed
AndySAUNA <andy.sibaja.amador@est.una.ac.cr>
2026-03-24 15:34:23 -0600

    Ver 0.1.1
    proceso de agregar clases, se ha agregado un menu de inicio

--- TareaProg2/src/main/java/cr/ac/una/tareaprog2/controller/MainController.java
+++ TareaProg2/src/main/java/cr/ac/una/tareaprog2/controller/MainController.java
@@ -6,7 +6,11 @@ package cr.ac.una.tareaprog2.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +19,17 @@ import javafx.fxml.Initializable;
  */
 public class MainController implements Initializable {
 
+    @FXML
+    private Button btnAdministrador;
+    @FXML
+    private Button btnFuncionario;
+    @FXML
+    private Button btnKiosk;
+    @FXML
+    private Button btnPantalla;
+    @FXML
+    private Button btnSalir;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +37,26 @@ public class MainController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onActionBtnAdministrador(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnFuncionario(ActionEvent event) {
+    }
+
+    @FXML
+    private void OnActionBtnKiosk(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnPantalla(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnSalir(ActionEvent event) {
+        ((Stage)btnSalir.getScene().getWindow()).close();
+    }
     
 }
